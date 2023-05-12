99ed73fcfa6a6690a8bd6c8a5c3933cf06a9b3fd
Huệ Nguyễn Tấn <20521359@ms.uit.edu.vn>
2023-05-13 00:29:45 +0700

    fix link

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -6,10 +6,13 @@ const userController = require('../controllers/userController');
 //o day chuyen den router de get va post data trong mongo
 router.get('/verify',verifyToken,userController.getUser)
 router.get('/userRole',userController.getUserRole)
+router.get('/customers', userController.getAllCustomers)
+router.get('/getStaff',userController.getStaff)
+
+
 router.get('/',userController.getAllUser)
 router.get('/:id',userController.getOneUser)
 
-router.get('/getStaff',userController.getStaff)
 
 router.post('/login',userController.Login)
 router.post('/register',userController.Register)
@@ -20,5 +23,4 @@ router.delete('/:id',userController.deleteUserFromId)
 router.post('/cart/:id',userController.addCart)
 router.delete('/cart/:id',userController.deleteCart)
 
-router.get('/customers', userController.getAllCustomers)
 module.exports = router
